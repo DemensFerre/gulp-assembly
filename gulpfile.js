75f26577feec25ddecf13b359a30adab92419768
Max <87299631+DemensFerre@users.noreply.github.com>
2022-12-29 01:31:56 +0300

    Project architecture changed
    also added several plugins for working with scss/sass/css and general plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,28 @@
 const { series, parallel, src, dest, watch } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const browserSync = require("browser-sync").create();
+const autoPrefixer = require("gulp-autoprefixer");
+const rename = require("gulp-rename");
+const cssnano = require("gulp-cssnano");
+const plumber = require("gulp-plumber");
+const del = require("del");
 
 const path = {
   src: {
     styles: `./src/scss/**/*.scss`,
+    html: `./src/html/*.html`,
   },
-  put: {
-    styles: `./css`,
+  build: {
+    styles: `./build/css`,
+    html: `./build`,
   },
 };
 
 const styles = () => {
   return src(path.src.styles)
-    .pipe(sass().on("error", sass.logError))
-    .pipe(dest(path.put.styles));
+    .pipe(sass())
+    .pipe(autoPrefixer())
+    .pipe(dest(path.build.styles));
 };
 
 exports.default = function () {
