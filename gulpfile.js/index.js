a7952c4cf9ae78117fc88591a9ae1bf15503aaa5
Max <thewildstar.m@gmail.com>
2023-01-06 14:45:11 +0300

    added basic styles

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,11 +1,11 @@
 global.$ = {
-  gulp: require("gulp"),
-  browserSync: require("browser-sync").create(),
-  plumber: require("gulp-plumber"),
-  notify: require("gulp-notify"),
-  gulpIf: require("gulp-if"),
-  path: require("./config/path.js"),
-  app: require("./config/app.js"),
+	gulp: require("gulp"),
+	browserSync: require("browser-sync").create(),
+	plumber: require("gulp-plumber"),
+	notify: require("gulp-notify"),
+	gulpIf: require("gulp-if"),
+	path: require("./config/path.js"),
+	app: require("./config/app.js"),
 };
 
 const clear = require("./tasks/clear");
@@ -17,15 +17,15 @@ const img = require("./tasks/img");
 const fonts = require("./tasks/fonts");
 
 const watcher = () => {
-  $.gulp.watch($.path.watch.html, html);
-  $.gulp.watch($.path.watch.styles, styles);
-  $.gulp.watch($.path.watch.js, js);
-  $.gulp.watch($.path.watch.img, img);
+	$.gulp.watch($.path.watch.html, html);
+	$.gulp.watch($.path.watch.styles, styles);
+	$.gulp.watch($.path.watch.js, js);
+	$.gulp.watch($.path.watch.img, img);
 };
 
 const build = $.gulp.series(
-  clear,
-  $.gulp.parallel(html, styles, js, img, fonts)
+	clear,
+	$.gulp.parallel(html, styles, js, img, fonts)
 );
 const dev = $.gulp.series(build, $.gulp.parallel(watcher, server));
 
